92a31bd1987bc6b67f62bcaa70a0fb3ce7ddc464
Stefan Schärmeli <schaermu@gmail.com>
2016-12-01 14:27:07 +0100

    cleans up obsolete boilerplate code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,29 +2,20 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
-
+import { Home } from '../pages/home/home'
 
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
+export class SpotalysisApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Page1;
+  rootPage: any = Home;
 
   pages: Array<{title: string, component: any}>;
 
   constructor(public platform: Platform) {
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
-    ];
-
   }
 
   initializeApp() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { SpotalysisApp } from './app.component';
+import { Home } from '../pages/home/home';
 
 @NgModule({
   declarations: [
-    MyApp,
-    Page1,
-    Page2
+    SpotalysisApp,
+    Home
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(SpotalysisApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    Page1,
-    Page2
+    SpotalysisApp,
+    Home
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
